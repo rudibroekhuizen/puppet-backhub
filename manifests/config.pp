579b1e9e7fca63067ad7f81109618eba28d4b848
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-20 16:28:16 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,10 @@
 #
 class backhub::config {
 
+# Get configs with scp using geoffwilliams/scp module
+
+
+
 # Setup logrotate using module yo61/logrotate
   logrotate::rule { 'backhub':
     path         => '/home/backhub/*',
