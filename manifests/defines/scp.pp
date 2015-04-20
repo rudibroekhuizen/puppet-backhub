2374af7daed498ac0bc46df21ba8315828b0c5a5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-20 23:09:24 +0200

    Update scp.pp

--- manifests/defines/scp.pp
+++ manifests/defines/scp.pp
@@ -5,7 +5,12 @@ define backhub::scp (
   $source,
   $schedule,
 ) {
-
+  
+  schedule { 'everyday':
+    period => daily,
+    range  => "2-4"
+  }
+  
   # Get configs with scp using geoffwilliams/scp module
   scp { $hostorip:
     ensure   => present
