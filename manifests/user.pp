8d0887fa709371920eb084032e52fa3a001ed0d2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-20 15:50:52 +0200

    Remove public key part

--- manifests/user.pp
+++ manifests/user.pp
@@ -39,16 +39,7 @@ class backhub::user {
     require => File["/home/backhub/.ssh"],
     }
 
-# Add public key
-  ssh_authorized_key { backhub:
-    ensure  => present,
-    user    => "backhub",
-    type    => ssh-rsa,
-    key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQClZqmBO9TEHXq1PcHvCqg7QEaRvNwftEYKk0s2XHBiiIkr6SStO7sX6DCTVXRFpB5CbsJHnJgYr3aeHWgBGd7dLugAuP+MWizMfECD7r3qcig3XKpAAXVf9siLRIb4mG6ABG4l5PJ9MiAqExljEx96YBbQvmmp85uECBlAhQnvzV+SMJ1485qLbRRtwxhnq5yxh/L6sZhEjcJJMBI7GwwwgLhXTWxRmkNT4Ba8vQAfR+V6lHVXQDwf1yjaNVa9m5vpWzNROpZqC05rYBuc9AooFQRTyYJyDyQc+QTqivdIcKMNBY0EWD1ssFqTuEtl+vyhg5JoqYrG2F2jDSD1vqq/",
-    require => File["/home/backhub/.ssh/authorized_keys"],
-  }
-
-# Add private key
+# Add private key to be able to scp to switches
   file { "/home/backhub/.ssh/id_rsa":
     owner   => "backhub",
     group   => "backhub",
