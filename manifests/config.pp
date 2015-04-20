a2526bff512022128cc1ab90d9168d38748b1be9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-20 23:04:40 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,7 @@ class backhub::config {
 
 # Get configs with scp using geoffwilliams/scp module
 scp { "/home/backhub/005438":
-  ensure   => present
+  ensure   => present,
   source   => "fred@build.mycompany.com/var/jenkins/data/myapp_current.war",
   schedule => 'everyday'
 }
