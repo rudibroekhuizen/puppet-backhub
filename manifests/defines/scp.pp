21bc1b6efc39569f8ac2b9cb559ebc71fe7f73e6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-30 20:13:09 +0200

    Change parameter repeat to run

--- manifests/defines/scp.pp
+++ manifests/defines/scp.pp
@@ -3,7 +3,7 @@
 define backhub::defines::scp (
   $save_as,
   $source,
-  $repeat,
+  $run,
   ) {
   
 $timestamp = strftime("%Y%m%d%H%M")
@@ -11,7 +11,8 @@ $timestamp = strftime("%Y%m%d%H%M")
 # Define schedule: run everynight between 2 and 4 am
   schedule { 'everyday':
     period => daily,
-    range  => "2-4"
+    range  => "2-4",
+    repeat => 1
   }
   
 # Get configs with scp using geoffwilliams/scp module
@@ -19,7 +20,7 @@ $timestamp = strftime("%Y%m%d%H%M")
     ensure   => present,
     source   => $source,
     name     => "/home/backhub/${timestamp}_${title}_${save_as}",
-    schedule => $repeat,
+    schedule => $run,
   }
   
 }
