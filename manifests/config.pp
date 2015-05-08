fc17ea0805b2da28e5ad50d1b759f0794b188945
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-08 12:23:30 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,13 @@
 #
 class backhub::config {
 
+# Allow clear text password logins, to be able to use the backhub account to push files to backhub server
+  augeas { "sshd_config":
+    changes => [
+      "set /files/etc/ssh/sshd_config/PasswordAuthentication yes",
+    ],
+  }
+
 # Setup logrotate using module yo61/logrotate
   logrotate::rule { 'backhub':
     path         => '/home/backhub/*',
